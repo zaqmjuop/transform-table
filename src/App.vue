5d0b1a023afe698ba026b3f51eba3c2f67ad3b09
朱博超 <1060007045@qq.com>
2022-10-07 18:19:47 +0800

    chore: add examples

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
-
 <template>
   <div id="app">
-    transform-table
+    <div>
+      <label v-for="(item, index) in EXAMPLES" :key="index">
+        <span> Example{{ index + 1 }}</span>
+        <input
+          type="radio"
+          name="exampleTarget"
+          :value="index"
+          :checked="exampleTarget === index"
+          @input="exampleTarget = index"
+      /></label>
+    </div>
+    <div>
+      <component :is="EXAMPLES[exampleTarget]"></component>
+    </div>
   </div>
 </template>
 
-
 <script setup lang="ts">
+import Vue, { ref } from "vue";
+import Example1 from "@/components/Example1.vue";
+import Example2 from "@/components/Example2.vue";
+import Example3 from "@/components/Example3.vue";
+import Example4 from "@/components/Example4.vue";
+import Example5 from "@/components/Example5.vue";
+import Example6 from "@/components/Example6.vue";
+import Example7 from "@/components/Example7.vue";
+const EXAMPLES = [
+  Example1,
+  Example2,
+  Example3,
+  Example4,
+  Example5,
+  Example6,
+  Example7,
+];
+const exampleTarget = ref(0);
 </script>
 
-
-<style scoped>
-</style>
+<style scoped></style>
